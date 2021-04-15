019aa4d53c2c48037caf0e1de0cbffcc546dbce1
Marta Mihaylova <martamihaylovajs@gmail.com>
2021-04-16 00:14:17 +0300

    make it better

--- server/config/config.js
+++ server/config/config.js
@@ -2,13 +2,13 @@ const config = {
     development: {
         SECRET: 'pesho',
         DB_URL: 'mongodb+srv://admin:488690@cluster0.1qfu5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-        PORT: 5001,
+        PORT: process.env.PORT || 5001,
         SALT_ROUNDS: 10,
     },
     production: {
         SECRET: 'pesho',
         DB_URL: 'mongodb+srv://admin:488690@cluster0.1qfu5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-        PORT: 80,
+        PORT: process.env.PORT || 80,
         SALT_ROUNDS: 10,
     }
 };
--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('./config/config');
-const routes = require('./config/routes');
+// const routes = require('./config/routes');
 const app =  express();
 
 app.use(cors());
 require('./config/express')(app);
 require('./config/mongoose');
 require('./config/routes')(app);
-app.use(routes);
-// app.get('*', (req, res) => {
-//     console.log(req);
-//     res.json({data: 'Hello'});
-// });
+// app.use(routes);
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
 
 app.listen(config.PORT, console.log.bind(console, `Server listening on port ${config.PORT}...`));
